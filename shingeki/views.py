1f482a55dd0c38b793e582339076306e3aff773c
Ana Helouise <anahelouise.ss@gmail.com>
2024-10-04 17:28:57 -0300

    com css

---
+++ shingeki/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def my_view(request):
+    return HttpResponse("Uma teste string de resposta")
+
+def home(request):
+    return render(request, "home.html")
+
+def user_view(request, username):
+    return HttpResponse(f"Perfil do usuário: {username}")
+
+def root_view(request):
+    return HttpResponse("Estamos na Raiz. Porta 8000")
+
+def contexto(request):
+    context = {
+        'nome': 'Ana Helouise',
+        'idade': 20,
+        'hobbies': ['Ouvir musicas', 'Assistir Shingeki no Kyojin', 'Programar']
+    }
+    return render(request, 'contexto.html', context)
+
+def anahelouise(request):
+    return render(request, "anahelouise.html")
